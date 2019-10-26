fec579849d677bd7fecf012dc3fdbdbf3f7175b8
Petro Rogutskyi <petro@mykolab.com>
2019-10-26 23:58:38 +0200

    empty AccountController and first unit test

---
+++ Problem.CashWithdraw.Web.Tests/Controllers/AccountControllerTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Problem.CashWithdraw.Web.Controllers.Api;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Problem.CashWithdraw.Web.Tests.Controllers
+{
+    [TestClass]
+    public class AccountControllerTests
+    {
+        private readonly AccountController accountController = new AccountController();
+
+        [TestMethod]
+        public void ShouldReturnErrorForInvalidArgumentException()
+        {
+            // Arrange
+
+            // Act
+            var actualResult = this.accountController.WithdrawMoney(-5) as BadRequestResult;
+
+            // Assert
+            Assert.AreEqual(400, actualResult.StatusCode);
+        }
+    }
+}
---
+++ Problem.CashWithdraw.Web/Controllers/Api/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Problem.CashWithdraw.Web.Controllers.Api
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountController : ControllerBase
+    {
+        [HttpPut]
+        public IActionResult WithdrawMoney(int amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
